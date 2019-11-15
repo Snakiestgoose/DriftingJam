01561f4a9d679f62c907f9bba096add138a6ff8c
Aaron <hobogod3@gmail.com>
2019-11-15 17:53:39 -0500

    WORDS
    THEY WORK

---
+++ Assets/Scripts/Words/WordDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WordDisplay : MonoBehaviour
+{
+
+    public float fallSpeed = 1f;
+    public TextMeshProUGUI text;
+
+    public void SetWord(string word)
+    {
+        text.text = word;
+    }
+
+    public void RemoveLetter()
+    {
+        text.text = text.text.Remove(0, 1);
+        text.color = Color.red;
+    }
+
+    public void RemoveWord()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        transform.Translate(0f, -fallSpeed * Time.deltaTime, 0f);
+    }
+}
