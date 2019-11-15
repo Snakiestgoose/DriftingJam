1c948d25683034def86a7e9a7b09fd732a26e7b9
Aaron <hobogod3@gmail.com>
2019-11-15 00:42:38 -0500

    Basic Animation set

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rd2d;
+    public float speed;
+    private Vector3 change;
+
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rd2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+
+
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            rd2d.MovePosition(transform.position + (change * speed * Time.deltaTime));
+        }
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            anim.SetInteger("State", 4);
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            anim.SetInteger("State", 3);
+        }
+        if (change == Vector3.zero)
+        {
+            anim.SetInteger("State", 0);
+        }
         
+
     }
 }
